3a8195eec89449e86d454d3c281f392e0816b7a6
Jonathan <9288@holbertonstudents.com>
2024-06-17 16:15:23 +0000

    size of multiples oparande

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,8 +12,8 @@ long l;
 long long ll;
 float f;
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
-printf("Size of a int: %ld byte(s)", sizeof(i));
-printf("Size of a long int: %ld byte(s)", sizeof(l));
-printf("Size of a long long int: %ld byte(s)", sizeof(ll));
-printf("Size of a float: %ld byte(s)", sizeof(f));
+printf("Size of a int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(l));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 }
