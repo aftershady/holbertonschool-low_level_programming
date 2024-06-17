d7c369058f0461ec1f6c13512a717470f021667f
Jonathan <9288@holbertonstudents.com>
2024-06-17 13:26:58 +0000

    size of multiple of initializer

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,19 @@
-#!/bin/bash
+/**
+*main - return nothing
+*Description: ''
+*Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+char _char;
+int _int;
+long _long;
+long long _llong;
+float _float;
+printf("%zu", sizeof(_char));
+printf("%zu", sizeof(_int));
+printf("%zu", sizeof(_long));
+printf("%zu", sizeof(_llong));
+printf("%zu", sizeof(_float));
+}
