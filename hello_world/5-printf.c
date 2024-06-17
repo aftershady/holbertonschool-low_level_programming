6b63a0f92995e327209fe1dba68fc37cbeeb76d7
Jonathan <9288@holbertonstudents.com>
2024-06-17 12:28:32 +0000

    display with

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+/**
+*main - return nothing
+*Description: 'display a string in a printf'
+*Return: Always 0 (Success)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
