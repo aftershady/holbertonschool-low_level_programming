be2f7d6b73f8978ca64878e2136683cc72542bc8
Jonathan <9288@holbertonstudents.com>
2024-06-17 16:11:37 +0000

    size of multiples oparande

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,14 @@
 #include <stdio.h>
 int main(void)
 {
-printf("Size of a char: %li byte(s)\n", sizeof(char));
-printf("Size of a int: %li byte(s)", sizeof(int));
-printf("Size of a long int: %li byte(s)", sizeof(long));
-printf("Size of a long long int: %li byte(s)", sizeof(long long));
-printf("Size of a float: %li byte(s)", sizeof(float));
+char c;
+int i;
+long l;
+long long ll;
+float f;
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of a int: %ld byte(s)", sizeof(i));
+printf("Size of a long int: %ld byte(s)", sizeof(l));
+printf("Size of a long long int: %ld byte(s)", sizeof(ll));
+printf("Size of a float: %ld byte(s)", sizeof(f));
 }
