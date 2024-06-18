04d6902bb8532875a1cc9f0f04d68947f9243820
Jonathan <9288@holbertonstudents.com>
2024-06-18 12:52:05 +0000

    display alphabet with putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -11,14 +11,14 @@ int main(void)
 char alphabet = 'a';
 char lr = '\n';
 int i = 0;
-while(i!=26)
+while (i != 26)
 {
 	putchar(alphabet);
 	alphabet++;
 	i++;
 }
 putchar(lr);
-return 0;
+return (0);
 
 }
 
