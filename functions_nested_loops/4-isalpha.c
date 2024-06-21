747e1d037cf35537938085e6ca92503e54c85ce8
Jonathan <9288@holbertonstudents.com>
2024-06-21 09:26:32 +0000

    function if lowercase or capital, return 1

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,21 +3,20 @@
 
 /**
 *_isalpha - capital checker
-i
 *@c: letter
-*Description: check if your letter is a capital or not
-*Return: 0 if capital 1 if not
+*Description: if you letter is captial or lower case return 1
+*Return: 1 if capital or lowar case or 0
 */
 int _isalpha(int c)
 {
 int i = 0;
 while (i <= 25)
 {
-        if (c == 'a' + i || c == 'A' + i)
-        {
-        return (1);
-        }
-        i++;
+	if (c == 'a' + i || c == 'A' + i)
+	{
+	return (1);
+	}
+	i++;
 }
 return (0);
 }
