05628ce0bca5d439265aef77d7a5b8586e961441
Jonathan <9288@holbertonstudents.com>
2024-06-21 12:29:20 +0000

    function who print 1 0 or -1

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,30 +1,27 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
-*_isalpha - capital checker
-*@c: letter
-*Description: if you letter is captial or lower case return 1
-*Return: 1 if capital or lowar case or 0
+*print_sign - pair or impair
+*@n: number
+*Description: return a char and a
+*Return: 1 or 0 or -1
 */
 int print_sign(int n)
 {
-if (n%n == 0 && n > 0)
+if (n % n == 0 && n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (n%n == 0 && n == 0)
+else if (n % n == 0 && n == 0)
 {
 	_putchar('0');
 	return (0);
 }
-else 
+else
 {
 	_putchar('-');
 	return (-1);
 }
-
-
 return (0);
 }
