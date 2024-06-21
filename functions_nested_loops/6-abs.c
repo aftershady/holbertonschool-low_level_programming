1efea0dc1a6a591a922c63a86bd4ee8358b2fadd
Jonathan <9288@holbertonstudents.com>
2024-06-21 12:59:31 +0000

    function absolute number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+*print_sign - pair or impair
+*@n: number
+*Description: return a char and a
+*Return: 1 or 0 or -1
+*/
+int _abs(int)
+{
+if (int == 0)
+{
+return('0');
+}	
+if (int < )
+{
+return(int * (int-1));
+}
+else
+{
+return (int);
+}
+}
