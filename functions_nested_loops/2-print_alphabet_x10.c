ef7ada2bbf40b49fe92ac918539f9de6fb582698
Jonathan <9288@holbertonstudents.com>
2024-06-21 07:36:55 +0000

    alphabet function x10 added

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Print the alphabet x10
+*Description: This function prints the alphabet in lowercase 10 times.
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+int j = 0;
+int c = 97;
+while (j <= 9)
+{
+while (i != 26)
+{
+_putchar(c);
+c++;
+i++;
+if (i == 26)
+{
+_putchar('\n');
+i = 0;
+j++;
+}
+}
+}
+}
