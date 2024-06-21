f58f8e7be243e257d7ca2769433337b47ff9821d
Jonathan <9288@holbertonstudents.com>
2024-06-21 08:27:41 +0000

    function check in capital or not

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Print the alphabet x10
+*Description: This function prints the alphabet in lowercase 10 times.
+*Return: void
+*/
+int _islower(int c);
+{
+int i = 0;
+while (c != "z")
+{
+	if(c == 'a'+i)
+	{
+	return 1;
+	}
+	i++
+}
+return 0;
+}
+~   
