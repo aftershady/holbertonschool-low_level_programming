1c558ac078190d4caa66b98ad6f07bc1cb409f7a
Jonathan <9288@holbertonstudents.com>
2024-06-21 13:41:04 +0000

    function absolute value of a number

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 #include "main.h"
 /**
-*print_sign - pair or impair
-*@n: number
-*Description: return a char and a
-*Return: 1 or 0 or -1
+*_abs - abs number
+*@a: number
+*Description: return the absolute value of a number
+*Return: abs value
 */
 int _abs(int a)
 {
 if (a == 0)
 {	
-return('0');
+return(0);
 }	
-if (a < 0)
+else if (a < 0)
 {
 return(a * -1);
 }
