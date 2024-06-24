2c4156cbe432bef66d846a692f93eac075b97ade
Jonathan <9288@holbertonstudents.com>
2024-06-24 12:48:24 +0000

    print - x times you want

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_line -
+ * @n:  
+ * Description: 
+ * Return: n
+ */
+void print_line(int n)
+{
+	int i = 0;
+	while(i <= n)
+	{
+		_putchar('-');
+	}
+}
