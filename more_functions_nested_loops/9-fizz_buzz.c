32409c0bd6179766e389194c0683ccbade0c573f
Jonathan <9288@holbertonstudents.com>
2024-06-24 19:52:37 +0000

    display 1 to 100 with x3 = fizz and x5 = buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,28 @@
 
 int main(void)
 {
-	int i;
+        int i;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-	printf("\n");
-	return (0);
+        for (i = 1; i <= 100; i++)
+        {
+                if (i%3 == 0 && i%5 != 0)
+                {
+                        printf("Fizz ");
+                }
+                else if (i%5 == 0 && i%3 != 0)
+                {
+                        printf("Buzz ");
+                }
+                else if (i%3 == 0 && i%5 == 0)
+                {
+                        printf("FizzBuzz ");
+                }
+                else
+                {
+                        printf("%d", i);
+                }
+		printf(" ");
+        }
+        printf("\n");
+        return (0);
 }
