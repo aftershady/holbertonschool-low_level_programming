60b6d3ca0a91c08a4bd6b4c1d3e08a99c1d24f43
Jonathan <9288@holbertonstudents.com>
2024-06-24 13:40:38 +0000

    print diagonal of \

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,28 @@
 void print_diagonal(int n)
 {
 	int i;
-
+	char space = '\n';
+	char under = '\\';
 	if (n > 0)
 	{
-	for (i = 0; i < n; i++)
-	{
-		_putchar('\\');
-	}
-	_putchar('\n');
-	}
-	else if (n == 0 || n < 0)
-	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			if (n > 1)
+			{
+				_putchar(under);
+			}
+			else
+			{
+				while (i < n)
+				{	
+				_putchar(space);
+				_putchar(under);
+				}
+			}
+		}
+		else if (n == 0 || n < 0)
+		{
+			_putchar(space);
+		}
 	}
 }
