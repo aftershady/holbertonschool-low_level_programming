52f2fe9a0867eb6559ff032ceb2e64f9cd2e2923
Jonathan <9288@holbertonstudents.com>
2024-06-24 20:16:40 +0000

    in construction

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_triangle - triangle rectancle of #
+ * @size : size of triangle
+ * Description: square of # with x rows and cols
+ * Return: void
+ */
+void print_triangle(int size)
+{
+
+}
