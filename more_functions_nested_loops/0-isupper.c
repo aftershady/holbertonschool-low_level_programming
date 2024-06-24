ff90bc3666cca63af666a971ad081cceaedaa247
Jonathan <9288@holbertonstudents.com>
2024-06-24 07:51:54 +0000

    test

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+while (i<=26)
+{
+if (c == 'a' + i)
+{
+	return (0);
+}
+else if (c == 'A' + i)
+{
+	return (1);
+}
+i++;
+}
+}
