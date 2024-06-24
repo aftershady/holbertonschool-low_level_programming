591322320374ae4b50fb9b989095796c99b6a1b7
Jonathan <9288@holbertonstudents.com>
2024-06-24 12:14:05 +0000

    display 10 times 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@
  */
 void more_numbers(void)
 {
-        int i = 0;
-	int j = 0;
-
+        int i, j;
+	int dixaine = 49;
+	int unité = 48;
 	for (i = 0; i < 10; i++)
 	{
 		for(j = 0; j <= 14; j++)
@@ -17,9 +17,11 @@ void more_numbers(void)
 			if (j < 9)
 			{
 
-				_putchar((j/10) + '0'); 
+				_putchar((dixiaine); 
+				unité = 0;
 			}
-		_putchar((j%10) + '0');
+		_putchar(unité);
+		unité++;
 		}
 		_putchar('\n');
 	}	
