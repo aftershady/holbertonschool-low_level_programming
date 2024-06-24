171586d7beb6156c1f0e9ee6d8dd2860f7dbd3b2
Jonathan <9288@holbertonstudents.com>
2024-06-24 11:33:23 +0000

    test

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * more_numbers - 
+ * Description: 
+ * Return: void
+ */
+void more_numbers(void)
+{
+        int i = 0;
+	int j = 0;
+	int l = 0;
+	char dixaine = '0';
+	char unité = '0';
+
+	for (i = 0; i < 14; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 10)
+			{
+				l++;
+			}
+			if (l > 0)
+			{
+				putchar(l + '0');
+			{
+			putchar(j + 0):
+			
+		}
+}
