b3db51da312b912db7cac561eef5c2d37af21275
Jonathan <9288@holbertonstudents.com>
2024-06-24 17:23:32 +0000

    diag of \\ with n -1 spaces

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,18 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_diagonal - print diag of \
- * @n:  number of \
- * Description: print diag of \ with n spaces
- * Return: n
+ * print_diagonal - print diag
+ * @n: number of diag
+ * Description: print diag with n-1 spaces
+ * Return: 0
  */
 void print_diagonal(int n)
 {
 	int i, j;
+
 	if (n > 0)
 	{
-		for(i = 1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			for (j = 2; j <= i; j++)
 			{
