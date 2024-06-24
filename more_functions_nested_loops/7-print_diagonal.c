cd7a8f41175e7a1926c295cf53f9b746614c404c
Jonathan <9288@holbertonstudents.com>
2024-06-24 13:22:32 +0000

    print diagonal of \

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_diagonal -
+ * @n:  number
+ * Description:
+ * Return: n
+ */
+void print_diagonal(int n);
+{
+	if (n > 0)
+	{
+	for (int i = 0; i < n; i++)
+	{
+		_putchar('\');
+		_putchar('\n');
+	}
+	}
+	else if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+}
