dc466246d2f4e0429c677707c743e1cd03222ec5
Jonathan <9288@holbertonstudents.com>
2024-06-24 13:59:02 +0000

    print diagonal of \

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_diagonal -
- * @n:  number
- * Description:
+ * print_diagonal - print diag of \
+ * @n:  number of \
+ * Description: print diag of \ with n spaces
  * Return: n
  */
 void print_diagonal(int n)
@@ -12,21 +12,18 @@ void print_diagonal(int n)
 	int row = 0;
 	if (n > 0)
 	{
-		if (n > 0)
+		for (col = 0; col < n; col++)
 		{
-			for (col = 0; col < n; col++)
+			while (row <= n)
 			{
-				while (row <= n)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
+				_putchar(' ');
 			}
-		}
-		else
-		{
+			_putchar('\\');
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
