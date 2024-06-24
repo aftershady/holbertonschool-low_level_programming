b6275d21b082adda10b7811ed6764543595f5fd0
Jonathan <9288@holbertonstudents.com>
2024-06-24 17:00:18 +0000

    test

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,29 @@
  * Return: n
  */
 void print_diagonal(int n)
-{
+{	
+	int i = 0;
 	int cols = 0;
 	int row = 0;
 	while (cols <= n)
 	{
+		row = 0;
+
 		if (n > 0)
 		{
-			while (row <= n)
+			while (i <= cols)
 			{
 				_putchar(' ');
+				i++;
 			}
-			_putchar(92);
+			_putchar('\\');
 			_putchar('\n');
+			row++;
 		}
 		else
 		{
 			_putchar('\n');
 		}
+		cols++;
 	}
 }
