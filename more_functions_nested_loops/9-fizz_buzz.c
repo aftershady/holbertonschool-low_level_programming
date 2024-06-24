af4ebe282660dadbf93d488a6c6ce36512c0efd2
Jonathan <9288@holbertonstudents.com>
2024-06-24 19:13:51 +0000

    display 1 to 100 with x3 = fizz and x5 = buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+/**
+ * main - display 1 to 100
+ * Description: display 1 to 10 with x5 = buzz & x3 = fizz
+ * Return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i%3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (i%5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
