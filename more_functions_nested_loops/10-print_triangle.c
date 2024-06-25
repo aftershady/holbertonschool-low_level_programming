37aebd4bc245a0211a07a7e74446edb649bb5f43
Jonathan <9288@holbertonstudents.com>
2024-06-25 08:49:50 +0000

    test

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,5 +8,20 @@
  */
 void print_triangle(int size)
 {
+	int i, j, k, l = 0;
 
+	for (i = size; size > 0; size--)
+	{
+		for (j = size - 1; j > 0; j--)
+		{
+			putchar('n');
+		}
+		for(k = 0 ; k <= l; k++)
+		{
+			putchar('#');
+		}
+		k = 0;
+		l++;
+		putchar('\n');
+	}
 }
