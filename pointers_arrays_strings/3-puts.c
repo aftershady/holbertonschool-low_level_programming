97581259e2f41f8291647c3a73e5985a4ed3c131
Jonathan <9288@holbertonstudents.com>
2024-06-26 07:40:19 +0000

    test

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,10 +9,10 @@ void _puts(char *str)
 {
 	int counter = 0;
 
-	while(str[counter] != '\0')
+	while (str[counter] != '\0')
 	{
 		_putchar(str[counter]);
-		counter ++;
+		counter++;
 	}
 	_putchar('\n');
 }
