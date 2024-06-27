d03245803fdd55ca2ae8ed3d0f54aabb64cdbaae
Jonathan <9288@holbertonstudents.com>
2024-06-27 13:15:39 +0000

    test

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * Description: reverse a string with temp file
+ * @s: string
+ * Return: 0
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		_putchar(str[i]);
+		i++;
+		if (str[i] != '\0')
+		{
+			i++;
+		}
+	}	
+}
