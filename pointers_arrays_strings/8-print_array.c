e9f6d547d039f13585692d3f3afbd1c7d91c4348
Jonathan <9288@holbertonstudents.com>
2024-06-27 17:46:00 +0000

    print n cases of a one line 5 cases array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,14 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * puts_half - print half of string
- * Description: print the half of a sting strating by the middle
- * @str: string
+ * print_array - print n cases
+ * Description: print n definded cases of the array
+ * @a: pointer to array of int
+ * @n: number of case to display
  * Return: 0
 */
 void print_array(int *a, int n)
 {
 	int i = 0;
+
 	if (n > 0)
 	{
 		while (a[i] != a[n])
@@ -22,5 +24,4 @@ void print_array(int *a, int n)
 		}
 	}
 	printf("\n");
-	
 }
