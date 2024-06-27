6411ededb9dfc642fe53101238d57012d3612585
Jonathan <9288@holbertonstudents.com>
2024-06-27 20:21:56 +0000

    in construction

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy -
+ * Description: 
+ * @dest: 
+ * @src:
+ * Return: 0
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int size = 0;
+
+	while (src[size] != '\0')
+	{
+		size++;
+	}
+	while (i != size)
+	{
+		dest[i] = size[i];
+	}
+}
