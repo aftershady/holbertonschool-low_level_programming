b3153957f0c9dc8526865188e63ac3eaf06be51c
Jonathan <9288@holbertonstudents.com>
2024-06-27 13:58:09 +0000

    in construction

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts_half - print half of string
+ * Description: print the half of a sting strating by the middle
+ * @str: string
+ * Return: 0
+*/
+void puts_half(char *str)
+{
+int i;	
+while (str[i] != '\0')
+{
+	i++;
+}
+}
