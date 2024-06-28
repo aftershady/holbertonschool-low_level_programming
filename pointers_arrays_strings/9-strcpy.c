58b9d937b5ad14537c20ff8848c18f1337286d47
Jonathan <9288@holbertonstudents.com>
2024-06-28 07:55:18 +0000

    copy a string in a table of char

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strcpy -
- * Description: 
- * @dest: 
- * @src:
+ * _strcpy - cpy a string in another
+ * Description: copy string src into dest[98]
+ * @dest: array of char
+ * @src: unknown size string
  * Return: 0
 */
 char *_strcpy(char *dest, char *src)
