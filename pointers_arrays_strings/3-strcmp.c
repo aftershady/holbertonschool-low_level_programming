82dec934274bfed322319c9f3fe6eb8a7e036e89
Jonathan <9288@holbertonstudents.com>
2024-06-30 11:21:12 +0000

    test

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,8 +10,16 @@ int _strcmp(char *s1, char *s2)
 int i, comp;
 
 for(i = 0; s1[i] == s2[i]; i++)
-
+{
+	if (s1 = '\0')
+	{
+		return (0);
+	}
+}
+if (s1[i] != s2[1])
+{
 comp = s1[i] - s2[i];
+}
 
 return (comp);
 }
