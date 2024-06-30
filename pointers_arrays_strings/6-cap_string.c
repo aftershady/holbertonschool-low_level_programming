d347836a7c63ac1346fb833c798b9703a1311dd1
Jonathan <9288@holbertonstudents.com>
2024-06-30 17:25:52 +0000

    test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 		{
 			if (str[i] == 'a' + j)
 			{
-				if (str[i-1] == '.' ||  str[i-1] == ';' ||  str[i-1] == ' ' || str[i-1] == '!' || i == 0)
+				if (str[i-1] == '.' ||  str[i-1] == ';' ||  str[i-1] == ' ' || str[i-1] == '!' || i == 0 || str[i-1] == '\n')
 				{
         				str[i] = 'A' + j;
 				}
