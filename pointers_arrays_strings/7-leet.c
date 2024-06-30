61de85456135598b55553fc78df3613a2aa711cb
Jonathan <9288@holbertonstudents.com>
2024-06-30 19:26:12 +0000

    test

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,28 +7,18 @@
 char *leet(char *str)
 {
 	int i, j;
+	char tab1[6] = {'a', 'e', 'o', 't', 'l'};
+	char tab2[6] = {'A', 'E', 'O', 'T', 'L'};
+	char tab[5] = {'4', '3', '0', '7', '1'};
 	
 	for(i = 0; str[i] != '\0'; i++)
 	{
-		for(j = 0; str[i] == 'a' || str[i] == 'A'; j++)
+		for (j = 0; j < 6; j++)
 		{
-			str[i] = 4;
-		}
-		for(j = 0; str[i] == 'e' || str[i] == 'E'; j++)
-		{
-			str[i] = 3;
-		}
-		for(j = 0; str[i] == 'o' || str[i] == 'O'; j++)
-		{
-			str[i] = 0;
-		}
-		for(j = 0; str[i] == 't' || str[i] == 'T'; j++)
-		{
-			str[i] = 7;
-		}
-		for(j = 0; str[i] == 'l' || str[i] == 'L'; j++)
-		{
-			str[i] = 1;
+			if (str[i] == tab1[j] || str[i] == tab2[j])
+			{
+				str[i] == tab[j];
+			}
 		}
 	}
 	return (str);
