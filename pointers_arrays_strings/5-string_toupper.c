e79954369c54bb19c1ee5d382fd6eb549c027932
Jonathan <9288@holbertonstudents.com>
2024-06-30 16:00:07 +0000

    test

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+/**
+ * string_toupper
+ * @str:
+ * Description:
+ * Return:
+ */
+char *string_toupper(char *str)
+{
+int i, j, size;
+
+for (size = 0; str[size] != '\0'; size++)
+{}
+
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < 26; j++)
+	{
+		if (str[i] = 'a' + j)
+		{
+			str[i] = 'A' + j;
+		}
+	}
+j = 0;	
+}
+return (str);
+}
