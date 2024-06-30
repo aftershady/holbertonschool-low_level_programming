fdc82080dadcaa7e67f7ef2b8b006bcc1f048572
Jonathan <9288@holbertonstudents.com>
2024-06-30 11:12:12 +0000

    test

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,27 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, j, counter_s1, counter_s2, comp;
+int i, j, comp;
 
-for(i = 0, counter_s1 = 0; s1[i] != '\0'; i++)
-{
-counter_s1 += s1[i];
-}
-for(j = 0, counter_s2 = 0; s2[j] != '\0'; j++)
-{
-counter_s2 += s2[j];
-}
-if (counter_s1 == counter_s2)
-{
-	comp = 0;
-}
-else if (counter_s1 < counter_s2)
-{
-	comp = -15;
-}
-else
-{
-	comp = 15;
-}
+for(i = 0; s1[i] == s2[i]; i++)
+
+comp = s1[i] - s2[i];
+
 return (comp);
 }
