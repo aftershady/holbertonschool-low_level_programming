0ae92c1491b7889a6fc26de89eef810760698951
Jonathan <9288@holbertonstudents.com>
2024-07-02 12:09:30 +0400

    search similar char in two string include the null

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,27 @@
 /**
- * _strspn
- * @s
- * @accept
- * Description:
- * Return:
+ * _strspn - count similars char
+ * @s: first string
+ * @accept: second string
+ * Description: check similar char between 2 string
+ * Return: counter of similarities
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    int i, j;
-    int counter = 0;
+int i, j;
+unsigned int counter = 0;
 
-    for (i=0; accept[i] != '\0'; i++)
-    {
-        for(j = 0; s[j] != '\0'; j++)
-        {
-            if (s[j] == accept[i])
-            {
-                counter++;
-                break;
-            }
-        }
-        j = 0;
-    }
-    counter++;
-
-    return (counter);
+for (i = 0; accept[i] != '\0'; i++)
+{
+for (j = 0; s[j] != '\0'; j++)
+{
+if (s[j] == accept[i])
+{
+counter++;
+break;
+}
+}
+j = 0;
+}
+counter++;
+return (counter);
 }
