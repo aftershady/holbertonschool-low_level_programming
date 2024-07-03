adfbcc9edb7b9e6f68c8d9f783bf2db0602dbda1
Jonathan <9288@holbertonstudents.com>
2024-07-03 10:17:32 +0400

    get lenght of a prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,29 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j;
-unsigned int counter = 0;
+	int i, j;
+	unsigned int counter = 0;
+	int check = 0;
 
-for (i = 0; accept[i] != '\0'; i++)
-{
-for (j = 0; s[j] != '\0'; j++)
-{
-if (s[j] == accept[i])
-{
-counter++;
-break;
-}
-}
-j = 0;
-}
-counter++;
-return (counter);
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if  (accept[i] == s[i])
+			{
+				check++;
+				break;
+			}
+		}
+			if (check == 1)
+			{
+				counter++;
+				break;
+			}
+			else
+			{
+				break;
+			}
+	}
+	return (counter);
 }
