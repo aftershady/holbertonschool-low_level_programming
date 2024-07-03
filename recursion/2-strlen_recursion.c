cd55bcf450c5f5b5fcde4e8063e723ea650c288c
Jonathan <9288@holbertonstudents.com>
2024-07-03 16:41:22 +0400

    calculate and return size of a string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _strlen_recursion - return size of string
  * @s: string
@@ -13,12 +12,10 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 	{
 		return (size);
-		printf("size dans if %d\n", size);
 	}
 	else
 	{
 		size++;
-		printf("size dans else %d\n", size);
 		_strlen_recursion(s + 1);
 	}
 	return (size);
