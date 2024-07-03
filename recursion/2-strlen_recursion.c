080df0e5db4879a66755d058ec51956047e7d2d0
Jonathan <9288@holbertonstudents.com>
2024-07-03 16:54:17 +0400

    return the size of a string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,22 +2,17 @@
 /**
  * _strlen_recursion - return size of string
  * @s: string
- * Description: stack size of string in size with recursion
- * Return: size
+ * Description: stack size of string in integer with recursion
+ * Return: 1 + last incrementation or 0
  */
 int _strlen_recursion(char *s)
 {
-	static int size;
-
 	if (*s == '\0')
 	{
-		return (size);
+		return (0);
 	}
 	else
 	{
-		size++;
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (size);
-
 }
