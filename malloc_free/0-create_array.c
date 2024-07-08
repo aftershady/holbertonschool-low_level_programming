9cb30703db3bbb50601809f6a0be9c09e4628495
Jonathan <shady14@hotmail.fr>
2024-07-08 11:08:19 +0200

    create a array with size not definded and char not defined for initializer

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,6 +12,11 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+		if (size == 0)
+	{
+		printf("failed to allocate memory\n");
+		exit(1);
+	}
 	array = malloc(sizeof(char) * size);
 
 	for (i = 0 ; i < size; i++)
