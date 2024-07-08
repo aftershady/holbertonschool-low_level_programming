f95710925bd21bd8ecff6d4a595f9d03a3fc1fc9
Jonathan <shady14@hotmail.fr>
2024-07-08 14:55:34 +0200

    update

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,4 +1,5 @@
-#ifndef 
+#ifndef MAIN_H
+#define MAIN_H
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -6,3 +7,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int _putchar(char c);
+
+#endif
