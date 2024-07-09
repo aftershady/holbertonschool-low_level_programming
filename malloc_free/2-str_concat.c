381ed8536bcd17c2816454823e1124a543c4aca6
Jonathan <shady14@hotmail.fr>
2024-07-09 10:35:34 +0200

    free a 2d array

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,12 +13,6 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *array;
 
-	if (s1 == NULL)
-		return (s2);
-
-	if (s2 == NULL)
-		return (s1);
-
 	array = malloc(sizeof(char) * ((sizeof(s1) - 1) + sizeof(s2)));
 
 	for (i = 0; s1[i] != '\0'; i++)
