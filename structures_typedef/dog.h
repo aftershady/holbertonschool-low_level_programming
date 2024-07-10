2e18161d24adefa01c698a421bbda88b4878597b
Jonathan <shady14@hotmail.fr>
2024-07-10 17:48:47 +0200

    initialise name, age owner of a dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - initialise name, age owner of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: dtruct dog initialize a dog in a structure with 3 parameters:
+ * his name
+ * his age
+ * the name of his owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
