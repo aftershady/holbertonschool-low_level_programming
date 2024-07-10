033f6c1257a94157d35f8d43e9a59593fad7be2f
Jonathan <shady14@hotmail.fr>
2024-07-10 20:59:47 +0200

    take data and initialise a structure dog with name/age/owner

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,30 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strcpy - cpy a string in another
- * Description: copy string src into dest[98]
- * @dest: array of char
- * @src: unknown size string
- * Return: 0
-*/
-char *_strcpy(char *dest, char *src)
-{
-	int size = 0;
-	int i = 0;
-
-	while (src[size] != '\0')
-	{
-		size++;
-	}
-	while (i != size)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-/**
  * init_dog - take data and initialise a structure dog with name/age/owner
  * @d: initialisation structure of type dog
  * @name: name of dog
@@ -36,18 +12,7 @@ char *_strcpy(char *dest, char *src)
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(strlen(name) + 1);
-	if (d->name == NULL)
-	{
-	d->name = "";
-	}
-
-	d->owner = malloc(strlen(owner) + 1);
-	if (d->name == NULL)
-	{
-	d->owner = "";
-	}
-	_strcpy(d->name, name);
+	d->name, name;
 	d->age = age;
-	_strcpy(d->owner, owner);
+	d->owner, owner;
 }
