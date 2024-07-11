02bf84fadaacb5cbc006287a821bd54a69fcb822
Jonathan <shady14@hotmail.fr>
2024-07-11 21:29:06 +0200

    concatenate 2 string in another, the 2nd have n size

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * string_nconcat - concatenate 2 string in another, the 2nd have n size
  * @s1: string 1
@@ -12,35 +13,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
+	unsigned int size_1 = 0;
+	unsigned int size_2 = 0;
 	char *array;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	if (n > strlen(s2))
-	{
-		n = strlen(s2);
-	}
-	array = malloc(sizeof(char) * (strlen(s1) + n + 1));
+	if (s1 != NULL)
+	{size_1 = (strlen(s1)); }
+	else
+	{size_1 = 0; }
+
+	if (s2 != NULL)
+	{size_2 = (strlen(s2) + 1); }
+	else
+	{size_2 = 0; }
+
+	if (n > size_2)
+	{n = size_2; }
+
+	array = malloc(sizeof(char) * (size_1 + n));
 
 	if (array == NULL)
+	{return NULL; }
+
+	if (size_1 != 0)
 	{
-		return (NULL);
+		for (i = 0; i < size_1; i++)
+		{
+			array[i] = s1[i];
+		}
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	if (size_2 != 0)
 	{
-		array[i] = s1[i];
-	}
-	for (j = 0; j < n; i++, j++)
-	{
-		array[i] = s2[j];
+		for (j = 0; j < n && s2[j] != '\0'; i++, j++)
+		{
+			array[i] = s2[j];
+		}
 	}
 	array[i] = '\0';
-
 	return (array);
 }
