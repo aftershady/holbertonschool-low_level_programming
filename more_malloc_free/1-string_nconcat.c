9f4c26f7ec50c08d99cc93a03980f4fb5b417957
Jonathan <shady14@hotmail.fr>
2024-07-12 08:03:21 +0200

    concatenate 2 string in another, the 2nd have n size

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -36,12 +36,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	if (s1 != NULL)
+	for (i = 0; i < size_1; i++)
 	{
-		for (i = 0; i < size_1; i++)
-		{
-			array[i] = s1[i];
-		}
+		array[i] = s1[i];
 	}
 	for (j = 0; j < n && s2[j] != '\0'; i++, j++)
 	{
