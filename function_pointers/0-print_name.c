5b08f2c087bd772be3213218207ca4ca219b6e25
Jonathan <shady14@hotmail.fr>
2024-07-15 12:42:47 +0200

    add a condition if name is null

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - select how to print the name using funtion pointer
  * @name: string
@@ -8,5 +10,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL)
+	{
 	f(name);
+	}
 }
