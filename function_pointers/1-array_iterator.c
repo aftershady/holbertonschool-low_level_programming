67dbbb09155af80a642422fbcdab997545f847e8
Jonathan <shady14@hotmail.fr>
2024-07-15 15:34:55 +0200

    create a loop on function pointer on array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,6 +3,7 @@
  * array_iterator - create a loop on function pointer on array
  * @array: array of int
  * @size: size of array
+ * @action: function pointer
  * Description: do a loop with a function pointer to display an array
  * Return: nothing
  */
