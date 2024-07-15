955f17656946a26ef50a7503a8be6ef1bfc20c43
Jonathan <shady14@hotmail.fr>
2024-07-15 12:06:30 +0200

    select how to print the name using funtion pointer

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - select how to print the name using funtion pointer
+ * @name: string
+ * @f: pointer to function
+ * Description: initialise function with pointer to a function
+ * in his parameters
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
