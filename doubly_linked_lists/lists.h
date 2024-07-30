a95921b3b7f00ad6985ea9bad7e73c68c7055326
Jonathan <shady14@hotmail.fr>
2024-07-30 13:57:10 +0200

    add function who print a doubly linked list on the next way

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+
+/**
+ * struct dlistint_t - structure for creation of nodes
+ * @n: number who will travel in list
+ * @next: pointer to next node
+ * @prev: pointeur to previous node
+ */
+typedef struct dlistint_t
+{
+	int n;
+	struct dlistint_t *next;
+	struct dlistint_t *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+
+#endif
