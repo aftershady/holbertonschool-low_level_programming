b71c22244ae749a80804b20683b9fef55cebbb4e
Jonathan <shady14@hotmail.fr>
2024-07-30 10:48:17 +0200

    add function who free a singly linked list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -19,5 +19,6 @@ list_t *add_node(list_t **head, const char *str);
 char *strcpy(char *dest, const char *src);
 size_t strlen(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
 
