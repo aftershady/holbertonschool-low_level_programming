118feb91440df2f8c1c32c72d82bda9844ff9529
Jonathan <shady14@hotmail.fr>
2024-08-02 16:20:46 +0200

    correction of code, buffer deleted

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,35 +7,28 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t file, writed;
-	char *buffer;
+
 
 	if (filename == NULL)
 	{
 		return -1;
 	}
 
-	file = open(filename, O_WRONLY | O_CREAT, 0600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
 		return (0);
 	}
-	buffer = malloc(sizeof(text_content) + 1);
-	if (buffer == NULL)
-	{
-		close(file);
-		return (0);
-	}
-	if (buffer != NULL)
+
+	if (text_content != NULL)
 	{
 		writed = write(file, text_content, strlen(text_content));
 		if (writed == -1)
 		{
-			free(buffer);
 			close(file);
 			return (-1);
 		}
 	}
-	free(buffer);
 	close(file);
 
 
