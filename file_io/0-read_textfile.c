a6d238b4708c83b64fba49e8bc5852a9a55460fe
Jonathan <shady14@hotmail.fr>
2024-08-02 15:04:22 +0200

    add function who read a write a file in terminal

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * read_textfile - read a file and write it in terminal
+ * @filename: pointer on file
+ * @letters: numbers of character to read and print
+ * Return: number of bytes writed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t file, readed, writed;
@@ -12,9 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	file = open(filename, O_RDONLY);
-
 	if (file == -1)
 	{
 		return (0);
@@ -23,9 +26,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 	{
 		close(file);
-		return(0);
+		return (0);
 	}
-
 	readed = read(file, buffer, letters);
 	if (readed == -1)
 	{
@@ -33,7 +35,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file);
 		return (0);
 	}
-
 	writed = write(STDOUT_FILENO, buffer, readed);
 	if (writed == -1)
 	{
@@ -41,13 +42,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file);
 		return (0);
 	}
-
 	free(buffer);
 	close(file);
-
 	if (readed != writed)
 		return (0);
-
-
 	return (writed);
 }
