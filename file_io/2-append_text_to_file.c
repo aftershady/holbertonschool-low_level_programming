f8740c8f9ea570ee74856293e9ae8430657204e5
Jonathan <shady14@hotmail.fr>
2024-08-03 10:14:02 +0200

    correction of a return 0 in case of error

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 	{
-		return (0);
+		return (-1);
 	}
 
 	if (text_content != NULL)
