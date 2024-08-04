e3883fdcaf326f739095c07ad8b4c11331fbfcae
Jonathan <shady14@hotmail.fr>
2024-08-04 16:14:02 +0200

    return to irginal main

--- doubly_linked_lists/7-main.c
+++ doubly_linked_lists/7-main.c
@@ -23,7 +23,7 @@ int main(void)
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 8, 100000);
+    insert_dnodeint_at_index(&head, 5, 100000);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
